94059565098044cf0830625f28fcefafc6ad5406
jubadz <attignadjib@outlook.com>
2020-01-24 17:32:42 +0100

    hello fuck head

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -118,7 +118,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'producthunt/static/')
 ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
