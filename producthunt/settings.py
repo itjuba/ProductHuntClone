afaff0afe2ab72608aecbdd0bac075b6cbe0996a
jubadz <attignadjib@outlook.com>
2020-01-24 17:00:30 +0100

    s

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'producthunt/static/')
 ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
